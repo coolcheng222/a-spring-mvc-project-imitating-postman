53cb6991043f27a9145d844d6d9e395954d5b20f
tom_glb <aaa@aaa>
2021-03-04 19:24:46 +0800

    fix some bug

--- src/main/java/com/sealll/util/ResponseFormatter.java
+++ src/main/java/com/sealll/util/ResponseFormatter.java
@@ -1,10 +1,5 @@
 package com.sealll.util;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
